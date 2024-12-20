6cb00206f045d58efb6055355365bc9d8f288a80
Supriyo Mukherjee <supriyomukherjee09@example.com>
2024-12-20 16:17:49 +0530

    Docker workflow cred fix

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -13,8 +13,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-    client_id            = "client_id"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    client_secret        = "client_secret"  # Can also be set via `ARM_CLIENT_SECRET` environment variable.
-    subscription_id      = "subscription_id"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    tenant_id            = "tenant_id"  # Can also be set via `ARM_TENANT_ID` environment variable.
+    client_id            = "clientid"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    client_secret        = "clientsecret"  # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    subscription_id      = "subscriptionid"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    tenant_id            = "tenantid"  # Can also be set via `ARM_TENANT_ID` environment variable.
   }
