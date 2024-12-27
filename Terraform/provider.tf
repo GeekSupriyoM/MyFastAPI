a1f140ffdefbaf04b36828df5b0f497c24e756a5
Supriyo Mukherjee <supriyomukherjee09@example.com>
2024-12-28 00:37:12 +0530

    provider changed

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -17,4 +17,13 @@ provider "azurerm" {
   client_secret   = "clientsecret"   # Can also be set via `ARM_CLIENT_SECRET` environment variable.
   subscription_id = "subscriptionid" # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
   tenant_id       = "tenantid"       # Can also be set via `ARM_TENANT_ID` environment variable.
+  
+}
+
+provider "azuread" {
+
+  client_id       = "clientid"       # Can also be set via `ARM_CLIENT_ID` environment variable.
+  client_secret   = "clientsecret"   # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+  tenant_id       = "tenantid"       # Can also be set via `ARM_TENANT_ID` environment variable.
+
 }
