40c096bae590fc94aa77aa48eb7accbdc9b0875b
Supriyo Mukherjee <supriyomukherjee09@example.com>
2024-12-28 01:33:23 +0530

    life cycle policay added

--- Terraform/modules/ServicePrincipal/main.tf
+++ Terraform/modules/ServicePrincipal/main.tf
@@ -3,12 +3,20 @@ data "azuread_client_config" "current" {}
 resource "azuread_application" "azapp" {
   display_name = var.service_principal_name
   owners       = [data.azuread_client_config.current.object_id]
+
+   lifecycle {
+    ignore_changes = [ owners ]
+  }
 }
 
 resource "azuread_service_principal" "spn" {
   client_id                    = azuread_application.azapp.client_id
   app_role_assignment_required = true
   owners                       = [data.azuread_client_config.current.object_id]
+
+  lifecycle {
+    ignore_changes = [ owners ]
+  }
 }
 
 resource "azuread_service_principal_password" "spnpass" {
